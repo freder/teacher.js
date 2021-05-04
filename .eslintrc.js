300c2c674ff97eb98aac0fd144d91ab92628f594
Frederic Brodbeck <der.froedi@gmail.com>
2021-05-04 10:59:45 +0200

    linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
+	env: {
+		node: true,
+		browser: true,
+		commonjs: true,
+		es6: true
+	},
 	plugins: [
 		'@typescript-eslint',
 		'jest',
@@ -10,4 +16,12 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended',
 		'plugin:jest/recommended',
 	],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+
+		'@typescript-eslint/no-var-requires': 0,
+	}
 };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+export {};
+
+
 function main() {
 	console.log('👍');
 }
