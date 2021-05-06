d4e1ed994fcd42574e55e7e42c1afc8bbf2a7d57
Frederic Brodbeck <der.froedi@gmail.com>
2021-05-06 15:13:47 +0200

    compare bundle size to baseline

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,49 @@ require('dotenv').config({ path: dotenvPath });
 
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { BundleStatsWebpackPlugin } = require('bundle-stats-webpack-plugin');
+const { StatsWriterPlugin } = require('webpack-stats-plugin');
+
 
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const ouputDirName = 'dist';
 const clientPath = './src/client';
+
+
+const plugins = [
+	new HtmlWebpackPlugin({
+		title: 'teacher.solar',
+		template: `${clientPath}/index.html`,
+		minify: false,
+		inject: 'body',
+	}),
+
+	new webpack.EnvironmentPlugin([
+		'NODE_ENV',
+		'SERVER_PORT'
+	]),
+];
+if (NODE_ENV === 'production') {
+	plugins.push(
+		new BundleStatsWebpackPlugin({
+			outDir: '../stats',
+			// baseline: true,
+			stats: {
+				excludeAssets: [/stats/],
+			},
+		})
+	);
+	plugins.push(
+		new StatsWriterPlugin({
+			filename: '../stats/webpack-stats.json',
+			stats: {
+				all: true,
+				source: false,
+			},
+		})
+	);
+}
 
 
 module.exports = {
@@ -27,19 +65,7 @@ module.exports = {
 		extensions: ['.js', '.ts', '.jsx', '.tsx']
 	},
 
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: 'teacher.solar',
-			template: `${clientPath}/index.html`,
-			minify: false,
-			inject: 'body',
-		}),
-
-		new webpack.EnvironmentPlugin([
-			'NODE_ENV',
-			'SERVER_PORT'
-		]),
-	],
+	plugins: plugins,
 
 	devtool: (NODE_ENV === 'development')
 		? 'inline-source-map'
