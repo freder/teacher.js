f642421337317ee6e5c322c3e317e895e2db47e2
Frederic Brodbeck <der.froedi@gmail.com>
2021-05-13 16:38:49 +0200

    imported code from demo

--- src/client/audio.ts
+++ src/client/audio.ts
@@ -30,10 +30,30 @@ export async function initJanus(): Promise<JanusInstance> {
 		});
 	});
 
+	let mixertest: any;
+	let myid = null;
+	let webrtcUp = false;
+	const myroom = 1234; // Demo room
+	let myusername = null;
+	// let audioenabled = false;
+
 	janus.attach({
-		plugin: 'janus.plugin.echotest',
+		plugin: 'janus.plugin.audiobridge',
 		success: (pluginHandle) => {
-			console.log(pluginHandle);
+			mixertest = pluginHandle;
+			console.log(
+				'Plugin attached! (' + mixertest.getPlugin() +
+				', id=' + mixertest.getId() + ')'
+			);
+
+			const username = 'asdfasdf';
+			const register = {
+				request: 'join',
+				room: myroom,
+				display: username
+			};
+			myusername = username;
+			mixertest.send({ message: register});
 		},
 		error: (cause: string) => {
 			console.error(cause);
@@ -41,10 +61,112 @@ export async function initJanus(): Promise<JanusInstance> {
 		// consentDialog: (on) => {
 		// 	// e.g., Darken the screen if on=true (getUserMedia incoming), restore it otherwise
 		// },
-		// onmessage: (msg, jsep) => {
-		// 	// We got a message/event (msg) from the plugin
-		// 	// If jsep is not null, this involves a WebRTC negotiation
-		// },
+		iceState: (state) => {
+			console.log('ICE state changed to ' + state);
+		},
+		mediaState: (medium, on: boolean) => {
+			console.log(
+				'Janus ' + (on ? 'started' : 'stopped') +
+				' receiving our ' + medium
+			);
+		},
+		webrtcState: (on: boolean) => {
+			console.log(
+				'Janus says our WebRTC PeerConnection is ' +
+				(on ? 'up' : 'down') + ' now'
+			);
+		},
+		onmessage: (msg, jsep) => {
+			// We got a message/event (msg) from the plugin
+			// If jsep is not null, this involves a WebRTC negotiation
+			console.log(' ::: Got a message :::', msg);
+			const event = msg['audiobridge'];
+			console.log('Event: ' + event);
+			if (event) {
+				if (event === 'joined') {
+					// Successfully joined, negotiate WebRTC now
+					if (msg['id']) {
+						myid = msg['id'];
+						console.log('Successfully joined room ' + msg['room'] + ' with ID ' + myid);
+						if (!webrtcUp) {
+							webrtcUp = true;
+							// Publish our stream
+							mixertest.createOffer({
+								media: { video: false }, // This is an audio only room
+								success: (jsep) => {
+									console.log('Got SDP!', jsep);
+									const publish = { request: 'configure', muted: false };
+									mixertest.send({ message: publish, jsep: jsep });
+								},
+								error: (error) => {
+									console.error('WebRTC error:', error);
+								}
+							});
+						}
+					}
+					// Any room participant?
+					if (msg['participants']) {
+						const list = msg['participants'];
+						console.log('Got a list of participants:', list);
+						for (const f in list) {
+							const id = list[f]['id'];
+							const display = list[f]['display'];
+							const setup = list[f]['setup'];
+							const muted = list[f]['muted'];
+							console.log('  >> [' + id + '] ' + display + ' (setup=' + setup + ', muted=' + muted + ')');
+						}
+					}
+				} else if (event === 'roomchanged') {
+					// The user switched to a different room
+					myid = msg['id'];
+					console.log('Moved to room ' + msg['room'] + ', new ID: ' + myid);
+					// Any room participant?
+					if (msg['participants']) {
+						const list = msg['participants'];
+						console.log('Got a list of participants:', list);
+						for (const f in list) {
+							const id = list[f]['id'];
+							const display = list[f]['display'];
+							const setup = list[f]['setup'];
+							const muted = list[f]['muted'];
+							console.log('  >> [' + id + '] ' + display + ' (setup=' + setup + ', muted=' + muted + ')');
+						}
+					}
+				} else if (event === 'destroyed') {
+					// The room has been destroyed
+					console.warn('The room has been destroyed!');
+				} else if (event === 'event') {
+					if (msg['participants']) {
+						const list = msg['participants'];
+						console.log('Got a list of participants:', list);
+						for (const f in list) {
+							const id = list[f]['id'];
+							const display = list[f]['display'];
+							const setup = list[f]['setup'];
+							const muted = list[f]['muted'];
+							console.log('  >> [' + id + '] ' + display + ' (setup=' + setup + ', muted=' + muted + ')');
+						}
+					} else if (msg['error']) {
+						if (msg['error_code'] === 485) {
+							// This is a 'no such room' error: give a more meaningful description
+						} else {
+							console.error(msg['error']);
+						}
+						return;
+					}
+					// Any new feed to attach to?
+					if (msg['leaving']) {
+						// One of the participants has gone away?
+						const leaving = msg['leaving'];
+						console.log('Participant left: ' + leaving);
+					}
+				}
+			}
+			if (jsep) {
+				console.log('Handling SDP as well...', jsep);
+				mixertest.handleRemoteJsep({ jsep: jsep });
+			}
+		},
 		// onlocaltrack: (track, added) => {
 		// 	// A local track to display has just been added (getUserMedia worked!) or removed
 		// },
