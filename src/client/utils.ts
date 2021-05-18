e27a00291bbc17bd3a25efbc8e4102a2116b5dbc
Frederic Brodbeck <der.froedi@gmail.com>
2021-05-18 15:16:28 +0200

    removed old code

--- src/client/utils.ts
+++ src/client/utils.ts
@@ -5,11 +5,3 @@ export function getWikipediaTocUrl(wikiPageUrl: string): string {
 	const args = 'prop=sections&format=json';
 	return `${host}/w/api.php?action=parse&page=${page}&${args}`;
 }
-
-
-export function getWikipediaToc(
-	wikiPageUrl: string
-): Promise<Record<string, unknown>> {
-	const url = getWikipediaTocUrl(wikiPageUrl);
-	return fetch(url).then((res) => res.json());
-}
