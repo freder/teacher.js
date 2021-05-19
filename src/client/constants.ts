6ab3613c544d76255ed13c2a5f0318bef8a200ac
Frederic Brodbeck <der.froedi@gmail.com>
2021-05-19 14:15:19 +0200

    production server url

--- src/client/constants.ts
+++ src/client/constants.ts
@@ -1,4 +1,9 @@
-const serverPort = process.env.SERVER_PORT;
+// TODO: make this configurable
+const serverPort = (process.env.NODE_ENV === 'production')
+	? 777
+	: process.env.SERVER_PORT;
 const serverName = process.env.SERVER_NAME;
-const protocol = (process.env.NODE_ENV === 'production') ? 'https' : 'http';
+const protocol = (process.env.NODE_ENV === 'production')
+	? 'https'
+	: 'http';
 export const serverUrl = `${protocol}://${serverName}:${serverPort}`;
