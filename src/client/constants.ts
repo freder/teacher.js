2dac02ca800183a62b95478a9f4ff036c3ba73b1
Frederic Brodbeck <der.froedi@gmail.com>
2021-05-19 14:11:24 +0200

    use https in production

--- src/client/constants.ts
+++ src/client/constants.ts
@@ -1,3 +1,4 @@
 const serverPort = process.env.SERVER_PORT;
 const serverName = process.env.SERVER_NAME;
-export const serverUrl = `http://${serverName}:${serverPort}`;
+const protocol = (process.env.NODE_ENV === 'production') ? 'https' : 'http';
+export const serverUrl = `${protocol}://${serverName}:${serverPort}`;
