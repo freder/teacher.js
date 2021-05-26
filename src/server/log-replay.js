35993dfe23e5d5a5071309bf0fa9999cd1968773
Frederic Brodbeck <der.froedi@gmail.com>
2021-05-26 13:08:39 +0200

    simple script to parse and replay events

---
+++ src/server/log-replay.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const path = require('path');
+
+const R = require('ramda');
+
+
+function parseEntry(line) {
+	const parts = line.split(' ');
+	const ts = new Date(parts[0]);
+	const type = parts[1];
+	const data = JSON.parse(parts[2] || 'null');
+	return { ts, type, data };
+}
+
+
+function addDuration(entry, i, list) {
+	const { ts } = entry;
+	let timeSincePrevious = 0;
+	if (i > 0) {
+		timeSincePrevious = ts - list[i - 1].ts;
+	}
+	return { ...entry, timeSincePrevious };
+}
+
+
+// read file
+const logFilePath = path.resolve('log.txt');
+console.log(logFilePath);
+const content = fs.readFileSync(logFilePath).toString();
+
+// parse entries
+let entries = R.pipe(
+	R.split(/[\n\r]+/g),
+	R.map(R.trim),
+	R.filter((s) => s !== ''),
+)(content);
+entries = entries
+	.map(parseEntry)
+	.map(addDuration);
+
+// replay
+// simplest solution: start all timers at once
+let offset = 0;
+entries.forEach((entry) => {
+	const delay = offset + entry.timeSincePrevious;
+	setTimeout(
+		() => console.log(entry),
+		delay
+	);
+	offset = delay;
+});
