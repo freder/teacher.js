ef95ae6a96a49d6443f39554d292a2d35cd1e126
Frederic Brodbeck <der.froedi@gmail.com>
2021-06-17 14:44:32 +0200

    unfuck previous commit

--- babel.config.js
+++ babel.config.js
@@ -7,17 +7,5 @@ module.exports = {
 	plugins: [
 		'@babel/proposal-class-properties',
 		'@babel/proposal-object-rest-spread'
-	],
-
-	targets: {
-		node: '6.5'
-	}
+	]
 };
-
-// "browserslist": [
-// 	"defaults",
-// 	"last 2 versions",
-// 	"> 1%",
-// 	"not dead",
-// 	"not IE > 0"
-// ],
