9e5cc5bf9f2c3502d5c0298a9d205008bf0a0818
Frederic Brodbeck <der.froedi@gmail.com>
2021-06-17 12:13:33 +0200

    both, teacher.solar and hydrogen should produce es6 code now

--- babel.config.js
+++ babel.config.js
@@ -7,5 +7,17 @@ module.exports = {
 	plugins: [
 		'@babel/proposal-class-properties',
 		'@babel/proposal-object-rest-spread'
-	]
+	],
+
+	targets: {
+		node: '6.5'
+	}
 };
+
+// "browserslist": [
+// 	"defaults",
+// 	"last 2 versions",
+// 	"> 1%",
+// 	"not dead",
+// 	"not IE > 0"
+// ],
